bd7151a020108b07f87e4bc324aca3db3d6ce03f
stahldom <jeff@helpfulcomm.com>
2020-01-30 15:50:48 +0100

    modified SiteConfig for github publishing

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'MXC Documentation', // Title for your website.
   tagline: 'Complete guide to MXC for both developers and users',
-  url: 'docs.mxc.org', // Your website URL
+  url: 'mxc.wiki', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'documentation',
-  organizationName: 'MXCFoundation',
+  organizationName: 'MXC-Foundation',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
